5efcd3401854b47268e72e96a531f228e20e0db5
Andrzej Borzęcki <andrew.borzecki@gmail.com>
2016-12-10 23:32:17 +0100

    Added models

---
+++ Azure/AzureIoTHub/src/AzureIoTHub/Models/Device.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureIoTHub.Models
+{
+    public class Device
+    {
+        public int ID { get; set; }
+        public bool Connected { get; set; }
+        public bool Ready { get; set; }
+        public Sensor[] Sensors { get; set;}
+    }
+}
---
+++ Azure/AzureIoTHub/src/AzureIoTHub/Models/Sensor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureIoTHub.Models
+{
+    public class Sensor
+    {
+        public int ID { get; set; }
+        public bool Read { get; set; }
+        public bool Write { get; set; }
+        public string name { get; set; }
+        public string value { get; set; }
+    }
+}
